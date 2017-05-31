1a372df175bee6fb84a5662e72c0f8fefae0bd19
curtisgreene <curtis.j.w.greene@gmail.com>
2017-05-31 15:01:22 -0400

    little

---
+++ src/components/WatchlistsPage.js
@@ -0,0 +1,57 @@
+// Receive a Watchlist as a prop,
+// reach out to the API to find movies that are on that Watchlist based on the join table,
+// Render out a list of all the movies using the Movie-Card component
+// eventually something like this: this.state.movies.map( movie => <MovieCard movie={movie}/> )
+
+import { Link, Switch, Route } from 'react-router-dom'
+import React from 'react'
+import WatchlistShow from './WatchlistShow'
+import CreateList from './CreateList'
+import WatchlistEdit from './WatchlistEdit'
+import SearchForm from './SearchForm'
+import MovieList from './MovieList'
+import MovieShow from './MovieShow'
+
+export default function WatchlistsPage(props) {
+  const watchlistElements = props.watchlists.map((li,i) =>
+      <li key={li.id}><Link to={`/watchlists/${li.id}`}><h1>{li.name}</h1></Link></li>)
+
+   if (props.watchlists){
+    return(
+    <div>
+      <div className="col-md-4">
+        <CreateList handleCreateList={props.handleCreateList} />
+        <h3>All Watchlists</h3>
+        <ul>
+          { watchlistElements }
+        </ul>
+        <Switch>
+          <Route exact path="/search"
+                render={()=>
+            <div>
+              <SearchForm handleSearch={props.handleSearch}
+                          handleChange={props.handleChange} />
+              <MovieList watchlists={props.watchlists}
+                        movieResults={props.movieResults}
+                        handleAddMovie={props.handleAddMovie}/>
+            </div>}
+          />
+          <Route exact path="/watchlists/:id" render={ ({match}) => {
+            const watchlist = props.watchlists.find(watchlist => watchlist.id === parseInt(match.params.id))
+            return <WatchlistShow onDelete={props.handleDelete} watchlist={watchlist}/>
+          } }/>
+          <Route exact path="/watchlists/:id/edit" render={ ({match}) => {
+            const watchlist = props.watchlists.find(watchlist => watchlist.id === parseInt(match.params.id))
+            return <WatchlistEdit handleUpdateWatchlist={props.handleUpdateWatchlist} watchlist={watchlist} />
+          } }/>
+          <Route exact path="/movies/:id" render={ ({match}) => {
+            const movie = props.movies.find(movie => movie.id === parseInt(match.params.id))
+            return <MovieShow movie={movie}/>
+          } }/>
+        </Switch>
+      </div>
+    </div>
+    )
+  } else
+    return <div><h1>LOADING.....</h1></div>
+  }
---
+++ src/containers/WatchlistContainer.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { getWatchlists, createJoin, createList, editWatchlist, deleteWatchlist, getMovies } from '../api/indexRailsAPI'
+import { MDBapiCall } from '../api/indexMDB'
+import WatchlistsPage from '../components/WatchlistsPage'
+
+
+
+export default class WatchlistContainer extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      movieResults: false,
+      watchlists: [],
+      searchTerm: '',
+      movies: []
+    }
+  }
+
+
+  handleChange(e){
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  handleSearch(searchTerm) {
+    MDBapiCall(searchTerm)
+    .then( data => this.setState({ movieResults: data }))
+  }
+
+  handleCreateList(name, description) {
+    createList(name, description)
+    .then( watchlist => this.setState( prevState =>  ({ watchlists: [...prevState.watchlists, watchlist] }) ))
+    .then( () => this.props.history.push('/watchlists'))
+    .catch(e => console.log('error', e))
+  }
+
+
+  componentDidMount() {
+    getWatchlists()
+    .then( res => this.setState({
+      watchlists: res
+    }))
+    getMovies()
+   .then(res => this.setState ({
+     movies: res
+   }) )
+  }
+
+  handleAddMovie(movie, watchlistId){
+    createJoin(movie, watchlistId)
+    .then( updatedWatchlist => {
+      const newWatchlists = this.state.watchlists.map( wl => {
+        if (wl.id === updatedWatchlist.id ) {
+          return updatedWatchlist
+        } else { return wl }
+        })
+      this.setState({watchlists: newWatchlists})
+      }
+      )
+    }
+
+  handleUpdateWatchlist(id, name, description){
+    editWatchlist(id, name, description)
+    .then( updatedWatchlist => {
+      const newWatchlists = this.state.watchlists.map( wl => {
+        if (wl.id === updatedWatchlist.id ) {
+          return updatedWatchlist
+        } else { return wl }
+        })
+      this.setState({watchlists: newWatchlists})
+      }
+      )
+    }
+
+  handleDelete(id){
+    deleteWatchlist(id)
+    .then( deletedWatchlist => {
+      const newWatchlists = this.state.watchlists.filter( wl => {
+        if (wl.id === deletedWatchlist.id ) {
+          return false
+        } else { return true }
+        })
+      this.setState({watchlists: newWatchlists})
+      }
+      )
+    }
+
+  render() {
+    return (
+      <div>
+        <WatchlistsPage watchlists={this.state.watchlists}
+                        movies={this.state.movies}
+                        handleDelete={this.handleDelete.bind(this)}
+                        handleUpdateWatchlist={this.handleUpdateWatchlist.bind(this)}
+                        handleCreateList={this.handleCreateList.bind(this)}
+                        handleSearch={this.handleSearch.bind(this)}
+                        handleChange={this.handleChange.bind(this)}
+                        movieResults={this.state.movieResults}
+                        handleAddMovie={this.handleAddMovie.bind(this)}
+                      />
+      </div>
+    )
+  }
+}
